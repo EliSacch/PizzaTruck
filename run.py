4d5913a450e203b442825962f8445e578c8f9d2b
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-11-06 16:11:35 +0000

    Create function to validate input

--- run.py
+++ run.py
@@ -10,23 +10,47 @@ def type_intro(message, speed):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
-# Rnd of code from Learn Learn Scratch Tutorials  
+# Rnd of code from Learn Learn Scratch Tutorials 
+
+
+def validate_action(action, choices):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if it is outside of the range of choices.
+    """
+    try:
+        is_integer = int(action)
+        is_valid_choice = ((is_integer-1)) >= 0 and ((is_integer-1) < len(choices))
+
+        if (is_integer == False) or (is_valid_choice == False) :
+            raise ValueError("Please, enter the number corrisponding to the desired action (Example: 1)"
+            )
+    except ValueError as e:
+        print(f"\nInvalid data: {e}. Please try again.")
+        return False
+        
+    return True
+
 
 
 def display_main_options():
     """
     This function displays the main navigation options.
     """
+    while True:
+        options = ["Menu and Order", "View current order"]
+        print("\nOptions:\n")
+        for option in options:
+            prefix = options.index(option) + 1
+            print(f"{prefix}. {option}")
+        
+        choosen_action = input("\nEnter the number corresponding to the desired action: ")
+        
+        if validate_action(choosen_action, options):
+            print("Valid action")
+            break
 
-    options = ["Menu and Order", "View current order"]
-    print("\n\nOptions:\n")
-    for option in options:
-        prefix = options.index(option) + 1
-        print(f"{prefix}. {option}")
-    
-    choosen_option = input("\nEnter the number corresponding to the desired action: ")
-    
-    print(choosen_option)
 
 
 
@@ -38,10 +62,10 @@ def main():
     logo = file.read()
     file.close()
 
-    intro_message = "\n\nWelcome to Pizza Truck!\nCheck our menu and place your order."
+    intro_message = "\n\nWelcome to Pizza Truck!\nCheck our menu and place your order.\n"
 
-    #type_intro(logo, 0.0000000000001)
-    #type_intro(intro_message, 0.1)
+    type_intro(logo, 0.0000000000001)
+    type_intro(intro_message, 0.1)
 
     display_main_options()
 
