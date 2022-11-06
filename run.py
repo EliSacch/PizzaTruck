b702d2e8025f2583df2006e8e6794f9d6e47d7da
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-11-06 15:19:35 +0000

    Customize type_intro function to read logo file

--- run.py
+++ run.py
@@ -1,16 +1,28 @@
 import sys,time,os
 
-intro_message = "Hello, welcome to Pizza Truck"
-
-def type_intro(message):
+def type_intro(message, speed):
+    """
+    This function takes a message as input and prints it one char at a time.
+    It also takes the speed input to customize the typing speed.
+    """
 #The following code is from Learn Learn Scratch Tutorials
     for char in message:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(speed)
 # Rnd of code from Learn Learn Scratch Tutorials  
 
 def main():
-    type_intro(intro_message)
+    """
+    This function runs the progam.
+    """
+    file = open("logo.txt")
+    logo = file.read()
+    file.close()
+
+    intro_message = "\n\nWelcome to Pizza Truck!\nCheck our menu and place your order."
+
+    type_intro(logo, 0.000000001)
+    type_intro(intro_message, 0.1)
 
 main()
