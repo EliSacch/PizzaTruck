974ebd4b162508dbd6dbf370e34b0f94378c92f8
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-11-06 17:19:10 +0000

    Create function to display the menu and the order options

--- run.py
+++ run.py
@@ -13,6 +13,43 @@ def type_intro(message, speed):
 # Rnd of code from Learn Learn Scratch Tutorials 
 
 
+def main_menu():
+    """ 
+    Calls the display options function to displays the main menu options.
+    Validates the input to check if it's a valid action.
+    Runs a sifferent function based on the returned action.
+    """
+    intro_message = "\n\nWelcome to Pizza Truck!\nCheck our menu and place your order.\n"
+    type_intro(intro_message, 0.05)
+
+    menu_options = ["Menu and Order", "View current order"]
+    action = display_options(menu_options)
+    if action == 1:
+        show_order_options()
+    if action == 2:
+        display_current_order()
+
+
+def display_options(options):
+    """
+    This function displays the options for the related section.
+    """
+    while True:
+        print("\nOptions:\n")
+        
+        for option in options:
+            prefix = options.index(option) + 1
+            print(f"{prefix}. {option}")
+        
+        choosen_action = input("\nEnter the number corresponding to the desired action: ")
+        
+        if validate_action(choosen_action, options):
+            break
+
+    return int(choosen_action) 
+
+    
+
 def validate_action(action, choices):
     """
     Inside the try, converts all string values into integers.
@@ -32,26 +69,54 @@ def validate_action(action, choices):
         
     return True
 
-
-
-def display_main_options():
+def display_menu():
     """
-    This function displays the main navigation options.
+    This function shows the available pizzas
     """
-    while True:
-        options = ["Menu and Order", "View current order"]
-        print("\nOptions:\n")
-        for option in options:
-            prefix = options.index(option) + 1
-            print(f"{prefix}. {option}")
-        
-        choosen_action = input("\nEnter the number corresponding to the desired action: ")
-        
-        if validate_action(choosen_action, options):
-            print("Valid action")
-            break
+    pizzas = [
+        "Margherita(White dough, tomato sauce, mozzarella chese, basil)", 
+        "Vegan(Wholegrain dough, tomato sauce, vegan cheese, mushrooms",
+        "Spicy{White dough, bbq sauce, mozzarella cheese, pepperoni",
+        "Truffle(White dough, mozzarella cheese, mushrooms, black truffle",
+        "Make your own"]
+
+    print("\nMenu:\n")
+
+    for pizza in pizzas:
+        number = pizzas.index(pizza) + 1
+        menu = f"{number}- {pizza}\n"
+        type_intro(menu, 0.001)
+
+    show_order_options()
+    
+    
+    
+def show_order_options():
+    """
+    This functions displays order options
+    """
+    menu_options = [
+        "Show menu",
+        "Add pizza",
+        "Remove pizza",
+        "Finish",
+        "Main menu"
+    ]
+
+    action = display_options(menu_options)
+    if action == 1:
+        display_menu()
+    if (action == 5):
+        main_menu()
+    else:
+        print("Other action")
 
 
+def display_current_order():
+    """
+    This function displays the items currently added to the order.
+    """
+    print("Here is the current order:")
 
 
 def main():
@@ -62,11 +127,9 @@ def main():
     logo = file.read()
     file.close()
 
-    intro_message = "\n\nWelcome to Pizza Truck!\nCheck our menu and place your order.\n"
+    #type_intro(logo, 0.0000000000001)
 
-    type_intro(logo, 0.0000000000001)
-    type_intro(intro_message, 0.1)
+    main_menu()
 
-    display_main_options()
 
 main()
