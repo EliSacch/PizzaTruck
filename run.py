80a01201ac7934c691577330c6cab643762835d0
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-11-06 14:04:26 +0000

    Add function to typewrite welcome message

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys,time,os
+
+intro_message = "Hello, welcome to Pizza Truck"
+
+def type_intro(message):
+#The following code is from Learn Learn Scratch Tutorials
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.1)
+# Rnd of code from Learn Learn Scratch Tutorials  
+
+def main():
+    type_intro(intro_message)
+
+main()
