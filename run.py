0aaf26194d439cc66db314b5a237bcffa9549d00
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-11-12 11:07:15 +0000

    Fix problems identified by pycodestyle

--- run.py
+++ run.py
@@ -1,27 +1,32 @@
-import sys,time,os
+import sys
+import time
 
 # GENERAL FUNCTIONS
+
 
 def type_intro(message, speed):
     """
     This function takes a message as input and prints it one char at a time.
     It also takes the speed input to customize the typing speed.
     """
-#The following code is from Learn Learn Scratch Tutorials
+# The following code is from Learn Learn Scratch Tutorials
     for char in message:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
-# Rnd of code from Learn Learn Scratch Tutorials 
+# Rnd of code from Learn Learn Scratch Tutorials
 
 
 def main_menu():
-    """ 
+    """
     Calls the display options function to displays the main menu options.
     Validates the input to check if it's a valid action.
     Runs a sifferent function based on the returned action.
     """
-    INTRO_MESSAGE = "\n\nWelcome to Pizza Truck!\nCheck our menu and place your order.\n"
+    file = open("logo.txt")
+    INTRO_MESSAGE = file.read()
+    file.close()
+
     type_intro(INTRO_MESSAGE, 0.05)
 
     menu_options = ["Menu and Order", "View current order"]
@@ -38,17 +43,14 @@ def display_options(options):
     """
     while True:
         print("\nOptions:\n")
-        
         for option in options:
             prefix = options.index(option) + 1
             print(f"{prefix}. {option}")
-        
-        choosen_action = input("\nEnter the number corresponding to the desired action: ")
-        
+        choosen_action = input("\nChoose an action(enter the action number): ")
         if validate_action(choosen_action, options):
             break
 
-    return int(choosen_action) 
+    return int(choosen_action)
 
 
 def validate_action(action, choices):
@@ -59,20 +61,20 @@ def validate_action(action, choices):
     """
     try:
         is_integer = int(action)
-        is_valid_choice = ((is_integer-1)) >= 0 and ((is_integer-1) < len(choices))
+        action = (is_integer-1)
+        is_valid_choice = action >= 0 and action < len(choices)
 
-        if (is_integer == False) or (is_valid_choice == False) :
-            raise ValueError("Please, enter the number corrisponding to the desired action (Example: 1)"
-            )
+        if is_integer is False or is_valid_choice is False:
+            raise ValueError("Please, enter the number corrisponding action")
     except ValueError as e:
         print(f"\nInvalid data: {e}. Please try again.")
         return False
-        
+
     return True
 
 
 # ORDER RELATED FUNCTIONS
-      
+
 def show_order_options():
     """
     This functions displays order options
@@ -86,7 +88,7 @@ def show_order_options():
     ]
 
     pizzas = [
-        "Margherita (White dough, tomato sauce, mozzarella chese, basil)", 
+        "Margherita (White dough, tomato sauce, mozzarella chese, basil)",
         "Vegan (Wholegrain dough, tomato sauce, vegan cheese, mushrooms",
         "Spicy {White dough, bbq sauce, mozzarella cheese, pepperoni",
         "Truffle (White dough, mozzarella cheese, mushrooms, black truffle",
@@ -120,22 +122,21 @@ def show_menu(pizzas):
 
 def add_pizza(pizzas):
     """
-    This function takes one input from the user, corresponding to the pizza they want to order.
+    This function takes one integer as input from the user.
+    The integer needs to correspond to the pizza they want to order.
     It adds the pizza to the crrent_order array.
     """
-    current_order = []
+    add_to_current_order = []
 
     while True:
-        choosen_pizza = input("\nEnter the number corresponding to the desired pizza: ")
+        choosen_pizza = input("\nAdd pizza: ")
         if int(choosen_pizza) == 0:
             print("Finish")
             break
         if validate_action(choosen_pizza, pizzas):
-            print(f"choosen: {choosen_pizza}, {pizzas[int(choosen_pizza)-1]}")
+            add_to_current_order.append(pizzas[int(choosen_pizza)-1])
 
-            #current_order.append(pizzas(choosen_pizza))
-
-    #return int(choosen_action) 
+    return add_to_current_order
 
 
 # BASKET AND CHECKOUT RELATED FUNCTIONS
@@ -157,7 +158,7 @@ def main():
     logo = file.read()
     file.close()
 
-    #type_intro(logo, 0.0000000000001)
+    # type_intro(logo, 0.0000000000001)
 
     main_menu()
 
